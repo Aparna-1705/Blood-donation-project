1a7a907511108fc6cf3582a2d393deb631e3e1b0
Aparna-1705 <aparnasreekumaran2269@gmail.com>
2026-03-03 21:59:56 +0530

    Update donorService.js

--- frontend/src/services/donorService.js
+++ frontend/src/services/donorService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/donors",
+  baseURL: "https://blood-donation-project-dxur.onrender.com/api/donors",
 });
 
 // ===============================
